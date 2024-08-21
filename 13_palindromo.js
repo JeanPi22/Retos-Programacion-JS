63e582318f32e9937a45f21e3b59ee2b6c540718
Jean Pierre Peña <jean.photo.grafico@gmail.com>
2024-08-20 21:05:54 -0500

    Reto 13 palindromo resuelto

---
+++ 13_palindromo.js
@@ -0,0 +1,28 @@
+/*
+Escribe una función que reciba un texto y retorne verdadero o
+falso (Boolean) según sean o no palíndromos.
+Un Palíndromo es una palabra o expresión que es igual si se lee
+de izquierda a derecha que de derecha a izquierda.
+NO se tienen en cuenta los espacios, signos de puntuación y tildes.
+Ejemplo: Ana lleva al oso la avellana.
+ */
+
+const phrase = 'Yo hago yoga hoy'
+
+function palindrome (phrase) {
+  
+  //Convertir texto en minúscula y quitar espacios.
+  const separateChar = phrase.toLocaleLowerCase().replace(/ /g, "")
+
+  //Invertir texto
+  const phraseReverse = separateChar.split("").reverse().join("");
+  
+  //Verifico que sean iguales
+  const isPalindrome = separateChar === phraseReverse
+  return isPalindrome
+  
+}
+
+console.log(palindrome('Yo hago yoga hoy'));
+console.log(palindrome('Ana lleva al oso la avellana'));
+console.log(palindrome('Esto no es un palíndromo'));
